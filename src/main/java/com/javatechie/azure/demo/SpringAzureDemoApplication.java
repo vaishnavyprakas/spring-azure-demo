0c5da733d21918ea11a7d807cdf0b79488f30484
vaishnavyprakas <vaishnavy.prakas@tigeranalytics.com>
2024-05-09 22:56:04 +0530

    Update SpringAzureDemoApplication.java

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")
+	@GetMapping()
 	public String message(){
 		return "Congrats ! your application deployed successfully in Azure Platform.This is finished !";
 	}
