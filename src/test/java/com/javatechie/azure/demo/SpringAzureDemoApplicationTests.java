93422c204ba4c486490c0769158a118468d6665e
vaishnavyprakas <vaishnavy.prakas@tigeranalytics.com>
2024-05-13 17:10:41 +0530

    Update SpringAzureDemoApplicationTests.java

--- src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
+++ src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.javatechie.azure.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
