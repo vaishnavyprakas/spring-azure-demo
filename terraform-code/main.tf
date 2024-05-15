81cdf8a360771f2a6b52cbb30e39abe7e58553d0
Vaishnavy Prakash <vaishnavy.prakas@tigeranalytics.local>
2024-05-15 21:57:09 +0530

    reset code

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -28,7 +28,7 @@ data "azurerm_service_plan" "service_plan" {
   name                = "demowebappava"
   resource_group_name = "demowebapprg"
 }
-######
+
 resource "azurerm_linux_web_app" "example" {
   name                = "demovjavawebapp"
   resource_group_name = data.azurerm_resource_group.rg.name
