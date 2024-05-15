8f183d32befec295b8be9f1f0eb3692c2dbff5ec
Vaishnavy Prakash <vaishnavy.prakas@tigeranalytics.local>
2024-05-15 21:50:49 +0530

    reset code

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping()
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform with private network access.";
+		return "Congrats ! your application deployed successfully in Azure Platform with private network access.!!";
 	}
 
 	public static void main(String[] args) {
